19907fe96b9b21def425cb998ca0defb961ac737
leosh1d <73224837+leosh1d@users.noreply.github.com>
2021-03-20 22:06:49 +0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -43,7 +43,7 @@ let login1, login2;
 const app = express();
 
 const corsOptions = {
-  origin: "https://leosh1d.github.io/full-stack/",
+  origin: "https://leosh1d.github.io/full-stack",
   credentials: true,
 };
 
